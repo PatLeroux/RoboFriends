a27eb092ae01fc159cd6eb3c1668c390716610fe
PatLeroux <leroux.patrick@icloud.com>
2022-05-30 16:00:17 -0400

    Updates

---
+++ precache-manifest.b4e361fed162376a372e7d83c92ec04a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a1713d22690654d7158",
+    "url": "/RoboFriends/static/css/main.60fc0271.chunk.css"
+  },
+  {
+    "revision": "3a1713d22690654d7158",
+    "url": "/RoboFriends/static/js/main.240a4c34.chunk.js"
+  },
+  {
+    "revision": "c01ddd688eb4a528c043",
+    "url": "/RoboFriends/static/js/runtime~main.84a40e82.js"
+  },
+  {
+    "revision": "205af7a98393f5236c91",
+    "url": "/RoboFriends/static/css/2.31f2379f.chunk.css"
+  },
+  {
+    "revision": "205af7a98393f5236c91",
+    "url": "/RoboFriends/static/js/2.e009e008.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/RoboFriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "70bcd8c46838140b91d7298f606d48de",
+    "url": "/RoboFriends/index.html"
+  }
+];
